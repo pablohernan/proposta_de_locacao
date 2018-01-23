67d4290cca513df388e5f31c9696e201eeaa73c6
User <user@gmail.com>
2018-01-23 12:02:06 -0200

    autoupdate`date +T`

--- public/js/grid.js
+++ public/js/grid.js
@@ -6,7 +6,7 @@ function addLine(){
 	var lineNumber = $( "#accordion" ).find(".card").length + 1;
 	var grupo = $( "#grupo" ).val();
 	var centro = $( "#centro" ).val();
-	var valor = $( "#valor" ).val();
+	var valor_custo = $( "#valor_custo" ).val();
 
 	var line = '<div class="card">'+
     '<div class="card-header" id="heading'+lineNumber+'">'+
@@ -20,7 +20,7 @@ function addLine(){
       '<div class="card-body">'+
         '<p><b>Grupo de Pagamento: </b><span class="item1">'+grupo+'</span></p>'+
         '<p><b>Centro de Custo: </b><span class="item2">'+centro+'</span></p>'+
-        '<p><b>Valor: </b><span class="item3">'+valor+'</span></p>'+
+        '<p><b>Valor: </b><span class="item3">'+valor_custo+'</span></p>'+
       '</div>'+
     '</div>'+
   '</div>';
