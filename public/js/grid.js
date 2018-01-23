b7f54631e3ae48a0c059efcb367bca47232b525e
User <user@gmail.com>
2018-01-23 11:52:44 -0200

    Signed-off-by: User <user@gmail.com>

---
+++ public/js/grid.js
@@ -0,0 +1,30 @@
+
+
+
+function addLine(){
+
+	var lineNumber = $( "#accordion" ).find(".card").length + 1;
+	var grupo = $( "#grupo" ).val();
+	var centro = $( "#centro" ).val();
+	var valor = $( "#valor" ).val();
+
+	var line = '<div class="card">'+
+    '<div class="card-header" id="heading'+lineNumber+'">'+
+      '<h5 class="mb-0">'+
+        '<button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapse'+lineNumber+'" aria-expanded="false" aria-controls="collapse'+lineNumber+'">'+
+         ' Item #'+lineNumber+' '+
+        '</button>'+
+      '</h5>'+
+    '</div>'+
+    '<div id="collapse'+lineNumber+'" class="collapse" aria-labelledby="heading'+lineNumber+'" data-parent="#accordion">'+
+      '<div class="card-body">'+
+        '<p><b>Grupo de Pagamento: </b><span class="item1">'+grupo+'</span></p>'+
+        '<p><b>Centro de Custo: </b><span class="item2">'+centro+'</span></p>'+
+        '<p><b>Valor: </b><span class="item3">'+valor+'</span></p>'+
+      '</div>'+
+    '</div>'+
+  '</div>';
+
+  $( "#accordion" ).append( line );
+
+}
