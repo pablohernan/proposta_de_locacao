f4566260bf6a603b01d9b30e5bb3cfcf21c0de82
User <user@gmail.com>
2018-01-31 16:28:34 -0200

    autoupdate`date +T`

--- public/js/valida.js
+++ public/js/valida.js
@@ -10,7 +10,7 @@ function vl_data(data){ // 12/12/2012
 
 	// anterior
 	if(moment( data, 'DD/MM/YYYY').fromNow().indexOf('ago')>-1 && !(moment($('#VENCIMENTO_NOVO').val(), 'DD/MM/YYYY').fromNow().indexOf('hours ago')>-1)){
-		return 'Data não pode ser anterior ao dia de hoje';
+		return 'Não pode ser anterior ao dia de hoje';
 	}
 
 	return true;
