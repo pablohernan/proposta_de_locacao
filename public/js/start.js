4caa556985b190700fb72e307afc20b0fa472bb0
User <user@gmail.com>
2018-07-17 23:40:48 -0300

    autoupdate`date +T`

--- public/js/start.js
+++ public/js/start.js
@@ -5,15 +5,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
     console.log('##START##');
     var Promise = PipefyApp.Promise;
     console.log('##create card##');
-
-
-p = PipefyApp.init();
-p.modal({
-  url: 'popup.html',
-  height: '90%',
-  width: '90%',
-});
-
     PipefyApp.initCall({
       /*
       'card-badges': function(p, context) {
